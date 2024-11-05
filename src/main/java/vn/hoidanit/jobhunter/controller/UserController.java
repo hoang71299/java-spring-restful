fa8b37387c12597bc29b8b975d81b09465969a83
Hoang <huynhhoang712003@gmail.com>
2024-11-05 15:54:27 +0700

    update user

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,9 +1,12 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +29,26 @@ public class UserController {
   }
 
   @DeleteMapping("/user/{id}")
-  public String deleteNewUser(@PathVariable("id") long hoidanit) {
-    this.userService.handleDeleteUser(hoidanit);
+  public String deleteNewUser(@PathVariable("id") long id) {
+    this.userService.handleDeleteUser(id);
     return "hello";
+  }
+
+  @GetMapping("/user/{id}")
+  public User getUserById(@PathVariable("id") long id) {
+    User user = this.userService.fetchUserById(id);
+    return user;
+  }
+
+  @GetMapping("/user/")
+  public List<User> fetchAllUser() {
+    List<User> users = this.userService.fetchAllUser();
+    return users;
+  }
+
+  @PutMapping("/user")
+  public User updateUser(@RequestBody User user) {
+    User ericUser = this.userService.handleUpdateUser(user);
+    return ericUser;
   }
 }
--- src/main/java/vn/hoidanit/jobhunter/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/jobhunter/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import vn.hoidanit.jobhunter.domain.User;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
