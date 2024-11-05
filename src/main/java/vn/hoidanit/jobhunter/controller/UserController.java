16d8a7a1e630fc2bcbf534366a13883d853ee038
Hoang <huynhhoang712003@gmail.com>
2024-11-05 17:25:33 +0700

    throw exception

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
 
 @RestController
 public class UserController {
@@ -30,8 +32,16 @@ public class UserController {
     return ResponseEntity.status(HttpStatus.CREATED).body(ericUser);
   }
 
+  @ExceptionHandler(value = IdInvalidException.class)
+  public ResponseEntity<String> handleIdException(IdInvalidException idException) {
+    return ResponseEntity.badRequest().body(idException.getMessage());
+  }
+
   @DeleteMapping("/users/{id}")
-  public ResponseEntity<String> deleteNewUser(@PathVariable("id") long id) {
+  public ResponseEntity<String> deleteNewUser(@PathVariable("id") long id) throws IdInvalidException {
+    if (id >= 1500) {
+      throw new IdInvalidException("id không  lớn hon 1500");
+    }
     this.userService.handleDeleteUser(id);
     return ResponseEntity.ok("ericUser");
     // return ResponseEntity.status(HttpStatus.OK).body("ericUser");
@@ -39,6 +49,7 @@ public class UserController {
 
   @GetMapping("/users/{id}")
   public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+
     User fetchUser = this.userService.fetchUserById(id);
     return ResponseEntity.status(HttpStatus.OK).body(fetchUser);
   }
