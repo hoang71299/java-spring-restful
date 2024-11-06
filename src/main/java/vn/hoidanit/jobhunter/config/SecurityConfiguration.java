8c08dbd5c9a1f492090e5069d748006b7c212ce9
Hoang <huynhhoang712003@gmail.com>
2024-11-06 18:44:29 +0700

    Xử lý JWT Exception

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -16,6 +16,10 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
@@ -35,7 +39,8 @@ public class SecurityConfiguration {
   }
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain filterChain(HttpSecurity http,
+      CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
     http
         .csrf(c -> c.disable())
         .authorizeHttpRequests(
@@ -43,11 +48,27 @@ public class SecurityConfiguration {
                 .requestMatchers("/", "/login").permitAll()
                 .anyRequest().authenticated())
 
-        .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+        .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
+            .authenticationEntryPoint(customAuthenticationEntryPoint))
+
+        .exceptionHandling(
+            exceptions -> exceptions
+                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) // 401
+                .accessDeniedHandler(new BearerTokenAccessDeniedHandler()))
         .formLogin(f -> f.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
+  }
+
+  @Bean
+  public JwtAuthenticationConverter jwtAuthenticationConverter() {
+    JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    grantedAuthoritiesConverter.setAuthorityPrefix("");
+    grantedAuthoritiesConverter.setAuthoritiesClaimName("hoidanit");
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+    return jwtAuthenticationConverter;
   }
 
   @Bean
--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class AuthController {
     Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
     // create a token
     String accessToken = this.securityUtil.createToken(authentication);
-
+    SecurityContextHolder.getContext().setAuthentication(authentication);
     ResLoginDTO res = new ResLoginDTO();
     res.setAccessToken(accessToken);
     return ResponseEntity.ok().body(res);
