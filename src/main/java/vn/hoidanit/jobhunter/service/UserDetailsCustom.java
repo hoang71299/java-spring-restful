10a3e0cf98cc9238112723d159a17437ab7f923e
Hoang <huynhhoang712003@gmail.com>
2024-11-06 16:10:17 +0700

    debug code

--- src/main/java/vn/hoidanit/jobhunter/service/UserDetailsCustom.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserDetailsCustom.java
@@ -1,7 +1,6 @@
 package vn.hoidanit.jobhunter.service;
 
 import java.util.Collections;
-
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,17 +8,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import vn.hoidanit.jobhunter.util.error.IdInvalidException;
+
 @Component("userDetailsService")
-public class UserDetailService implements UserDetailsService {
+public class UserDetailsCustom implements UserDetailsService {
+
   private final UserService userService;
 
-  public UserDetailService(UserService userService) {
+  public UserDetailsCustom(UserService userService) {
     this.userService = userService;
   }
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     vn.hoidanit.jobhunter.domain.User user = this.userService.handleGetUserByUsername(username);
+    if (user == null) {
+      throw new UsernameNotFoundException("Username/password không hợp lệ");
+    }
 
     return new User(
         user.getEmail(),
